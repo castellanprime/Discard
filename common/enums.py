5a0931c663a67f19c82ae504e040e4e3f7f89d7e
Okusanya Damilola <okusanya.damilola@gmail.com>
2017-06-15 21:21:49 -0500

    Added enum representation

--- common/enums.py
+++ common/enums.py
@@ -1,36 +1,41 @@
 
 from enum import Enum
 
-class ShapeColour(Enum):
+class MyEnum(Enum):
+
+	def __str__(self):
+		return "({0}={1})".format(self.__class__.__name__, self.name)
+
+class ShapeColour(MyEnum):
 	RED = 1
 	BLUE = 2
 	GREEN = 3
 	YELLOW = 4
 
-class CardColour(Enum):
+class CardColour(MyEnum):
 	BLACK = 1
 	WHITE = 2
 
-class Shapes(Enum):
+class Shapes(MyEnum):
 	CROSS = 1
 	SQUARE = 2
 	TRIANGLE = 3
 	CIRCLE = 4
 	CUP = 5
 
-class PlayerState(Enum):
+class PlayerState(MyEnum):
 	PLAYING = 1					# Currently playing
 	PLAYED = 2					# Just played before currently playing
 	PAUSED = 3					# Waiting to play
 
-class GameState(Enum):
+class GameState(MyEnum):
 	WIN = 1						# Player won 
 	LOSE = 2					# Player lost
 	INVALIDMOVE = 3
 	NOACTION = 4
 	DRAW = 5 					# If the game is timed
 
-class CardType(Enum):
+class CardType(MyEnum):
 	SPECIAL=1
 	NORMAL=2
 
