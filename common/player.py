ad891d1a3bf5fe2204b5aae8176d63ae60db95db
Okusanya Damilola <okusanya.damilola@gmail.com>
2017-06-19 01:31:34 -0500

    Deleted giveupTurn() and fixed formatting

--- common/player.py
+++ common/player.py
@@ -9,11 +9,13 @@ class Player(object):
 		self.last_played = []
 		self.game = None
 		self.last_card = False
+		self.has_played_last_card = False
 
 	# This allows the player to join a game
 	def join(self, game):
 		self.game = game 
-		self._logger.info("Joined game")
+		sstrs = "Joined game - " + self.nick
+		self._logger.info(sstrs)
 
 	def get_nick_name(self):
 		return self.nick
@@ -24,29 +26,21 @@ class Player(object):
 	def get_last_played(self):
 		return self.last_played
 
-	# This is the card numbers to remove
-	# Refactor to allow for the players to retract their card/mistakes
-	# This ability should be the given to the game
 	def select_cards(self, card_numbers):
 		self.last_played = [self.deck.pop(card_number) for card_number in card_numbers]
+		sstr = "The length of the deck: " + str(len(self.deck))
+		self._logger.info(sstr)
 		if len(self.deck) == 1:
 			self.last_card = True
+		elif len(self.deck) == 0:
+			self.has_played_last_card = True
 
 	def add_a_card(self, card):
 		self.deck.append(card)
 
 	def pick_one(self, card):
+		self._logger.info(str(card))
 		self.deck.insert(0, card)
-
-	def give_up_turn(self):
-		#self.pick_one()
-		#self.game.set_current_player(self.game.get_next_turn())
-		pass
 
 	def __str__(self):
 		return "Player: {}".format(self.nick)
-
-
-
-
-	
